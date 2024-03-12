0ceca5d2d6da9eed3372c46946b1351620475887
Ashis13 <159094007+Ashis13@users.noreply.github.com>
2024-03-11 20:56:45 -0700

    added API
    * added axios link to html file
    * created function to fetch real temperature data from API and update app interface with the live data as input (city & temperature)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ function city(event) {
   let searchCityInput = document.querySelector("#search-city-input");
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = searchCityInput.value;
+
+  function weather(response) {
+    let temperature = Math.round(response.data.temperature.current);
+    let cityTemperature = document.querySelector(".temp-value");
+    cityTemperature.innerHTML = `${temperature}`;
+  }
+  let apiKey = "384o5eb54t8f21820fdceb7ff6b5a26b";
+  let query = searchCityInput.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weather);
 }
 let cityInput = document.querySelector("#search-city");
 cityInput.addEventListener("submit", city);
